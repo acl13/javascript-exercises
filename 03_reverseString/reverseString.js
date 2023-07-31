95a6609efa6615765defb5e06d486c29a02f905b
acl13 <128934431+acl13@users.noreply.github.com>
2023-07-31 15:31:16 -0400

    test code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,5 +2,7 @@ const reverseString = function(str) {
    return str.split("").reverse().join("");
 };
 
+console.log(reverseString("hippopotamus"));
+
 // Do not edit below this line
 module.exports = reverseString;
