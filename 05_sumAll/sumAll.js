671b856f271e34c546e6e2a5980bb384dfcbb46a
acl13 <128934431+acl13@users.noreply.github.com>
2023-07-31 16:26:47 -0400

    create sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if(a < 0 || b < 0 ||typeof a != "number" || typeof b != "number" ) {
+        return "ERROR";
+    }
+    else if(a < b) {
+        let sum = 0;
+        for(n = a; n <= b; n++){
+            sum = sum + n; 
+        }
+        return sum;
+    }
+    else if(b < a) {
+        let sum = 0;
+        for(n = b; n <= a; n++){
+            sum = sum + n;
+        }
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
 
 };
+
+console.log(sumAll(13,6));
 
 // Do not edit below this line
 module.exports = sumAll;
