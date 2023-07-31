0061eca9e3e3292ed1a62c3e64c0342e5990e64a
acl13 <128934431+acl13@users.noreply.github.com>
2023-07-31 14:58:36 -0400

    clean code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,24 +1,8 @@
 const removeFromArray = function(arr, ...theArgs) {
-   const newArray = arr.filter(function(element) {
-    return !theArgs.includes(element);
-}); 
-   return newArray;
-};
+   return arr.filter(element => !theArgs.includes(element));
+}; 
+   
 
-
-//let array = [1,2,3,4];
-
-console.log(removeFromArray([1,2,3,4], 2, 3));
-
-/*
-let arr = [1,2,3,4];
-const newArray = arr.filter(function(element) {
-    return element != 3;
-});
-
-console.log(newArray);
-
-*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
