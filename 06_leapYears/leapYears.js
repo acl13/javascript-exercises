8eeb29b88d328ed960a6e50db3296483b2f9e2af
acl13 <128934431+acl13@users.noreply.github.com>
2023-08-01 12:31:47 -0400

    create leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(n) {
+   if ((n % 4 === 0) && (n % 400 === 0)){
+    return true;
+   } else if ((n % 4 === 0) && (n % 100 != 0)){
+     return true;
+   } else {
+    return false;
+   }
+   
 };
 
+
+console.log(leapYears(1984));
 // Do not edit below this line
 module.exports = leapYears;
