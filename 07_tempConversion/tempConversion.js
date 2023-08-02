af4f09ccb4aad7d9597ba75778e8b8aa19a9ad6c
acl13 <128934431+acl13@users.noreply.github.com>
2023-08-02 15:08:31 -0400

    create tempConversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const Celcius = (temp - 32) * (5/9);
+  let result = Math.round(Celcius * 10)/10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const Fahrenheit = temp * (9/5) + 32;
+  let result = Math.round(Fahrenheit * 10)/10;
+  return result;
 };
+
 
 // Do not edit below this line
 module.exports = {
