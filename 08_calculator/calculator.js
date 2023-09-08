6c1a0fac1d982ad21cd9f70a92aa3bf8e21738b5
acl13 <128934431+acl13@users.noreply.github.com>
2023-09-08 15:29:27 -0400

    completed calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
+const add = function(a, b) {
+return a + b;
+}
+
+
+const subtract = function(a, b) {
+  return (a - b);
+};
+
+const sum = function(arr) {
+	return arr.reduce((acc, current) => acc + current, 0);
+};
+
+const multiply = function(array) {
+  return array.reduce((acc, current) => acc * current, 1);
+
+};
+
+const power = function(a, b) {
+  return a ** b;
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(num) {
+  let answer = 1;
+  if (num == 0 || num == 1) {
+    return answer;
+  } else {
+    for (let i = num; i >= 1; i--){
+      answer = answer * i;
+    }
+    return answer;
+  }
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
